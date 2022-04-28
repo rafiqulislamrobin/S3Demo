20a4c5c17163271372f630dec4e3878d5e293bc8
Rafiqul Iislam Robin <kratosrobin467@gmail.com>
2022-04-28 16:05:43 +0600

    file upload and download

---
+++ DemoProject/DemoProject/Areas/Admin/Controllers/FileController.cs
@@ -0,0 +1,89 @@
+﻿using DemoProject.Areas.Admin.Models.FileModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DemoProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FileController : Controller
+    {
+        private readonly ILogger<FileController> _logger;
+        private readonly IOptions<FileUploadSettings> _fileUploadSettings;
+        private readonly IHostEnvironment _environment;
+
+        public FileController(ILogger<FileController> logger,
+            IOptions<FileUploadSettings> fileUploadSettings,
+            IHostEnvironment Environment)
+        {
+            _logger = logger;
+            _fileUploadSettings = fileUploadSettings;
+            _environment = Environment;
+        }
+        
+
+        [HttpGet]
+        public IActionResult UploadFile()
+        {
+            var uploadFolder = _fileUploadSettings.Value.UploadFolder;
+            string wwwPath = _environment.ContentRootPath;
+            var model = new FileUploadModel();
+            model.GetAllFiles(wwwPath, uploadFolder);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile file)
+        {
+            var model = new FileUploadModel();
+
+            try
+            {
+                var uploadFolder = _fileUploadSettings.Value.UploadFolder;
+                string wwwPath = _environment.ContentRootPath;
+                model.UploadFile(file, uploadFolder);
+                model.GetAllFiles(wwwPath, uploadFolder);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "failed to upload file");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult DownloadFile(string fileName, string extentionType)
+        {
+            try
+            {
+                var uploadFolder = _fileUploadSettings.Value.UploadFolder;
+                var download = new FileDownloadModel();
+                var file = download.Download(fileName, extentionType, uploadFolder);
+                return File(file.bytes, file.extentionType, file.filename);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "failed to download file");
+                return RedirectToAction(nameof(UploadFile));
+            }
+
+        }
+
+        public IActionResult Delete(string fileName)
+        {
+            try
+            {
+                var uploadFolder = _fileUploadSettings.Value.UploadFolder;
+                var model = new FileDownloadModel();
+                model.DeleteFile(fileName, uploadFolder);
+                return RedirectToAction(nameof(UploadFile));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "failed to delete file");
+                return RedirectToAction(nameof(UploadFile));
+            }
+        }
+
+    }
+}
---
+++ DemoProject/DemoProject/Areas/Admin/Models/FileModels/FileUploadModel.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace DemoProject.Areas.Admin.Models.FileModels
+{
+    public class FileUploadModel
+    {
+        public IFormFile File { get; set; }
+        public List<FileInfo> Files { get; set; }
+
+        public void UploadFile(IFormFile file, string uploadFolder)
+        {
+            if (file is not null)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var contentType = file.ContentType;
+
+
+                if (contentType == "text/plain" || contentType == "application/json")
+                {
+                    using (FileStream stream = new FileStream(Path.Combine(uploadFolder, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+
+            }
+
+        }
+
+
+        public void GetAllFiles(string wwwPath, string uploadFolder)
+        {
+            string path = Path.Combine(wwwPath, uploadFolder);
+
+            if (!Directory.Exists(path))
+            {
+               var x = Directory.CreateDirectory(path);
+            }
+          
+            var files = Directory
+               .GetFiles(uploadFolder, "*.*")
+               .Where(file => file.ToLower().EndsWith("txt") || file.ToLower().EndsWith("json"))
+               .ToList();
+
+            Files = new List<FileInfo>();
+
+            foreach (string item in files)
+            {
+                FileInfo fi = null;
+
+                try
+                {
+                    fi = new System.IO.FileInfo(item);
+                    Files.Add(fi);
+                }
+                catch (Exception ex)
+                {
+                    throw new FileNotFoundException(ex.Message);
+                }
+            }
+        }
+    }
+}
